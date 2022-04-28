48c89245e5e230e403e00b6cad63dada90e32edf
gyane928 <45209312+gyane928@users.noreply.github.com>
2022-04-28 18:11:59 +0900

    Create connection.php

---
+++ phpfiles/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+//replace username with your phpmyadmin username
+//replace password with your phpmyadmin password
+// replacce database name with your database name 
+$conn = mysqli_connect("localhost", "verifyid", "Atlanta123*", "verifyid");
+
+if($conn)
+{
+  //echo "Connection Success";
+}
+else
+{
+  //echo "Connection Failed";
+
+}
+
+?>
